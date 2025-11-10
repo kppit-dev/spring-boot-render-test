07b65b32f638dc9ea098bfa50115c7ec78f348f9
momeh <momeh@host.docker.internal>
2025-11-10 21:20:10 +0800

    test

--- spring-kppit-render/src/main/java/com/kppit/web/SpringKppitRenderApplication.java
+++ spring-kppit-render/src/main/java/com/kppit/web/SpringKppitRenderApplication.java
@@ -2,9 +2,17 @@ package com.kppit.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SpringKppitRenderApplication {
+	
+	@GetMapping
+	public String getMessage() {
+		return "Welcome to KPPIT";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringKppitRenderApplication.class, args);
