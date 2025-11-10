c4ed74394ee958aa73d0bf4eb88c9986167df641
momeh <momeh@host.docker.internal>
2025-11-10 21:01:29 +0800

    init

---
+++ spring-kppit-render/src/main/java/com/kppit/web/SpringKppitRenderApplication.java
@@ -0,0 +1,13 @@
+package com.kppit.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringKppitRenderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringKppitRenderApplication.class, args);
+	}
+
+}
